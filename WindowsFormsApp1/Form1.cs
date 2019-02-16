4e0ea817f2bf1a36a452734d8d7bdc580099d469
Sinfogamo Gaahl <38165764+Sinfogamo@users.noreply.github.com>
2019-02-16 14:14:18 -0600

    funcionalidad de los botones

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,181 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            button1.Visible = false; //se ocultan los botones para que no marquen nada
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            button6.Visible = false;
+            button7.Visible = false;
+            button8.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex; //se toma el valor de la opcion del combobox
+            if (p != null)
+            {
+                button1.Visible = true; //se hacen visibles los botones para mostrar la informacion 
+                button2.Visible = true;
+                button3.Visible = true;
+                button4.Visible = true;
+                button5.Visible = true;
+                button6.Visible = true;
+                button7.Visible = true;
+                button8.Visible = true;
+            }
+
+            switch(p)
+            {
+                case 0: //se compara para saber que telefono es y asi mostrar la imagen 
+                    pictureBox1.Image = Properties.Resources.moto_g5;
+                break;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex;
+            switch (p)
+            {
+                case 0:
+                    Form imagen5 = new Form
+                    {
+                        Size = new Size(766, 402),
+                        MinimizeBox = false,
+                        MaximizeBox = false,
+                        BackgroundImage = Properties.Resources.camara
+                    };
+                    imagen5.Show();
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex;
+            switch (p)
+            {
+                case 0:
+                    Form imagen5 = new Form
+                    {
+                        Size = new Size(766, 402),
+                        MinimizeBox = false,
+                        MaximizeBox = false,
+                        BackgroundImage = Properties.Resources.bateria
+                    };
+                    imagen5.Show();
+                break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex;
+            switch (p)
+            {
+                case 0:
+                    Form imagen5 = new Form
+                    {
+                        Size = new Size(766, 402),
+                        MinimizeBox = false,
+                        MaximizeBox = false,
+                        BackgroundImage = Properties.Resources.procesador
+                    };
+                    imagen5.Show();
+                    break;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex;
+            switch (p)
+            {
+                case 0:
+                    Form imagen5 = new Form
+                    {
+                        Size = new Size(766, 402),
+                        MinimizeBox = false,
+                        MaximizeBox = false,
+                        BackgroundImage = Properties.Resources.camara
+                    };
+                    imagen5.Show();
+                    break;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex;
+            switch (p)
+            {
+                case 0:
+                    Form imagen5 = new Form
+                    {
+                        Size = new Size(766, 402),
+                        MinimizeBox = false,
+                        MaximizeBox = false,
+                        BackgroundImage = Properties.Resources.camara
+                    };
+                    imagen5.Show();
+                    break;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex;
+            switch (p)
+            {
+                case 0:
+                    Form imagen5 = new Form
+                    {
+                        Size = new Size(766, 402),
+                        MinimizeBox = false,
+                        MaximizeBox = false,
+                        BackgroundImage = Properties.Resources.pantalla
+                    };
+                    imagen5.Show();
+                    break;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex;
+            switch (p)
+            {
+                case 0:
+                    Form imagen5 = new Form
+                    {
+                        Size = new Size(766, 402),
+                        MinimizeBox = false,
+                        MaximizeBox = false,
+                        BackgroundImage = Properties.Resources.camara
+                    };
+                    imagen5.Show();
+                    break;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int p = comboBox1.SelectedIndex;
+            switch (p)
+            {
+                case 0:
+                    Form imagen5 = new Form
+                    {
+                        Size = new Size(766, 402),
+                        MinimizeBox = false,
+                        MaximizeBox = false,
+                        BackgroundImage = Properties.Resources.camara
+                    };
+                    imagen5.Show();
+                    break;
+            }
         }
     }
 }
